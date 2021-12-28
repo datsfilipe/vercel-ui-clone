252ffb6ae20bf118653c5d9623de84aa2efe06ea
datsfilipe <datsfilipe@gmail.com>
2021-12-28 00:10:57 -0300

    add navItemHighlight component

---
+++ src/components/NavItemHighlight/index.tsx
@@ -0,0 +1,20 @@
+import styles from './styles.module.scss'
+
+interface NavItemHighlightProps {
+  width: number;
+  translateX: number;
+}
+
+export function NavItemHighlight ({ width, translateX }: NavItemHighlightProps) {
+  return (
+    <div
+      className={styles.container}
+      style={{
+        width: `${width}px`,
+        transform: `translateX(${translateX}px)`
+      }}
+    />
+  )
+}
+
+// 20.7
