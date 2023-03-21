cf318d373f46315397f4a962c3c02302b80298bd
datsfilipe <datsfilipe.pro@proton.me>
2023-03-21 19:42:01 -0300

    feat: add env var for development

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const baseURL = process.env.NOT_DEV ? 'https://vercel-ui-clone-three.vercel.app' : 'http://localhost:3000'
+
 export const api = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: baseURL,
 })
